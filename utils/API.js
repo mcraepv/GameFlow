515170703b1bb961c0d822e54f4153ee11db208c
mcraepv <61661042+mcraepv@users.noreply.github.com>
2020-09-17 22:04:21 -0500

    expands genres, fleshes out quiz JSON

--- utils/API.js
+++ utils/API.js
@@ -6,6 +6,20 @@ const steamApiKey = 'key=C4559EEF7DAF679ED65CF8FB368F5868';
 const rawgBaseURL = 'https://api.rawg.io/api/games?search=';
 const rawgHeader = { 'User-Agent': 'GameFlow' };
 
+// const getTags = async () => {
+//   const tags = await axios.get('https://api.rawg.io/api/tags?page_size=1000');
+
+//   let i = 0;
+
+//   tags.data.results.forEach((res) => {
+//     console.log(res.name);
+//     i++;
+//   });
+//   console.log(i);
+// };
+
+// getTags();
+
 module.exports = {
   getOwnedGames: async (id) => {
     const branch = 'IPlayerService/';
@@ -44,16 +58,19 @@ module.exports = {
         break;
       }
     }
-    const tagsArr = [];
+    const genresArr = [];
     if (result) {
       result.tags.forEach((tag) => {
         const regex = /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]+$/g;
         if (regex.test(tag.name)) {
-          tagsArr.push(tag.name);
+          genresArr.push(tag.name);
         }
       });
+      result.genres.forEach((genre) => {
+        genresArr.push(genre.name);
+      });
     }
-    return tagsArr;
+    return genresArr;
   },
 };
 
