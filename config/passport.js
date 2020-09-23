572fe6f159a5ec528abeb16978664f24f7cc92ac
mcraepv <61661042+mcraepv@users.noreply.github.com>
2020-09-22 19:08:46 -0500

    heroku pls

--- config/passport.js
+++ config/passport.js
@@ -10,17 +10,25 @@ module.exports = (passport) => {
     done(null, obj);
   });
 
+  const productionVars = {
+    returnURL: 'https://secret-wildwood-10554.herokuapp.com/auth/steam/return',
+    realm: 'https://secret-wildwood-10554.herokuapp.com/',
+    apiKey: 'C4559EEF7DAF679ED65CF8FB368F5868',
+  };
+
+  const developmentVars = {
+    returnURL: 'http://localhost:3001/auth/steam/return',
+    realm: 'http://localhost:3001/',
+    apiKey: 'C4559EEF7DAF679ED65CF8FB368F5868',
+  };
+
+  const stratVars =
+    process.env.NODE_ENV === 'production' ? productionVars : developmentVars;
+
   passport.use(
-    new SteamStrategy(
-      {
-        returnURL: 'http://localhost:3001/auth/steam/return',
-        realm: 'http://localhost:3001/',
-        apiKey: 'C4559EEF7DAF679ED65CF8FB368F5868',
-      },
-      async function (identifier, profile, done) {
-        const userGames = await usersController.create(profile.id);
-        done(null, userGames);
-      }
-    )
+    new SteamStrategy(stratVars, async function (identifier, profile, done) {
+      const userGames = await usersController.create(profile.id);
+      done(null, userGames);
+    })
   );
 };
